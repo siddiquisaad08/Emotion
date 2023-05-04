6ba7978303d0ad067599fe967d6c3e4f0fb7a3b6
mamoon-4g <112717860+mamoon-4g@users.noreply.github.com>
2023-05-04 14:33:38 +0530

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,9 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
+import 'youtube/screens/home_screen.dart';
+import 'dart:io';
+import 'manual.dart';
 
+// List<String> emotion = ['happiness','sadness', 'neutral', 'anger', 'surprise'];
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,75 +14,216 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  File? image;
-  String output = '';
+  File? _image;
+  String _output = '';
+  late Future<void> _modelLoaded;
 
   @override
   void initState() {
     super.initState();
-    loadModel();
+    _modelLoaded = loadModel();
   }
 
-  pickImage() async {
+  Future<void> pickImage(ImageSource camera) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
       setState(() {
-        image = File(pickedFile.path);
-        runModel();
+        _image = File(pickedFile.path);
       });
+      // Call the runModel() function after setting the image file
+      await runModel();
     }
   }
 
-  runModel() async {
-    if (image != null) {
-      var predictions = await Tflite.runModelOnImage(
-          path: image!.path,
-          numResults: 5,
-          threshold: 0.05,
-          imageMean: 127.5,
-          imageStd: 127.5);
+  Future<void> runModel() async {
+    if (_image != null) {
+      final List<dynamic>? results = await Tflite.runModelOnImage(
+        path: _image!.path,
+        numResults: 5,
+        threshold: 0.05,
+        imageMean: 127.5,
+        imageStd: 127.5,
+      );
 
-      predictions!.forEach((element) {
+      if (results != null) {
+// add a null check here
+
         setState(() {
-          output +=
-              '${element['label']}: ${(element['confidence'] as double).toStringAsFixed(2)}\n';
+          _output = results[0]['label'];
         });
-      });
+
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text(
+                'Emotion Detected',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              content: Text(
+                _output,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              backgroundColor: Colors.white,
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              actions: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeScreen2(_output),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Confirm',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 32.0,
+                    ),
+                    elevation: 8.0,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    pickImage(ImageSource.camera);
+                  },
+                  child: const Text(
+                    'Retry',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 32.0,
+                    ),
+                    elevation: 8.0,
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      }
     }
   }
 
-  loadModel() async {
+  Future<void> loadModel() async {
     await Tflite.loadModel(
-        model: "assets/model.tflite", labels: "assets/labels.txt");
+      model: "assets/newmodel.tflite",
+      labels: "assets/labels.txt",
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Emotion Based Music Player'),
+        backgroundColor: Color(0xFF3660DC),
+        elevation: 0,
+        title: Text('Emotion Detection'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.7,
-              width: MediaQuery.of(context).size.width,
-              child: image == null
-                  ? const Center(child: Text('No image selected'))
-                  : Image.file(image!),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => pickImage(),
-            child: const Text('Select Image'),
-          ),
-          const SizedBox(height: 20),
-          Text(output,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
-        ],
+      body: FutureBuilder<void>(
+        future: _modelLoaded,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.65,
+                    width: MediaQuery.of(context).size.width,
+                    child: _image == null
+                        ? const Center(child: Text('No image selected'))
+                        : Image.file(_image!),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () => pickImage(ImageSource.camera),
+                  child: const Text(
+                    'Select Image',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 32.0,
+                    ),
+                    elevation: 8.0,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => manual(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Use Emoji',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 32.0,
+                    ),
+                    elevation: 8.0,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
